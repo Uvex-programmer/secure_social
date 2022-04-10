a0caf7f100ce690c8087654865313ca29b48ed03
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2022-04-10 14:25:22 +0200

    created repos

--- src/main/java/com/example/demo/repositories/GroupRepository.java
+++ src/main/java/com/example/demo/repositories/GroupRepository.java
@@ -1,13 +1,12 @@
 package com.example.demo.repositories;
 
-import com.example.demo.models.ERole;
-import com.example.demo.models.Role;
+import com.example.demo.models.Group;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RoleRepository extends PagingAndSortingRepository<Role, String> {
-    Optional<Role> findByName(ERole name);
+public interface GroupRepository extends PagingAndSortingRepository<Group, String> {
+
 }
