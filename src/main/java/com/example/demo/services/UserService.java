17bf55e66d4add3023a2b58c924b610f62b0d2c3
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2022-04-10 14:25:54 +0200

    added User service with impl

---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,13 @@
+package com.example.demo.services;
+
+import com.example.demo.models.User;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    List<User> findAllUsers();
+    Optional<User> findOneById(ObjectId id);
+    List<User> findByIdIn(List<String>ids);
+}
